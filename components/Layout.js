31be559ebcc1da97e6dc2c2bdf27cf3b6c63dce0
Leroy Clarke Jr <lclarkejr94@gmail.com>
2021-08-11 00:15:57 -0400

    Fixing next-seo

--- components/Layout.js
+++ components/Layout.js
@@ -9,7 +9,7 @@ const Layout = ({ children }) => {
       <NextSeo
         title="Leroy Clarke Jr. | Front-end Engineer"
         description="Self taught Front-end Engineer helping ambitious people and brands create aesthetically pleasing web app experiences."
-        openGraph={{url: 'https://www.leroyclarkejr.com', title:'Leroy Clarke Jr. | Front-end Engineer', description:'Self taught Front-end Engineer helping ambitious people and brands create aesthetically pleasing web app experiences.', images: [{url: 'www.leroyclarkejr.com/images/leroyclarkejr.jpg', width='800',  height:'772', alt='Leroy Clarke Jr. in black and white.'}], site_name: 'LeroyClarkeJr'}}
+        openGraph={{url: 'https://www.leroyclarkejr.com', title:'Leroy Clarke Jr. | Front-end Engineer', description:'Self taught Front-end Engineer helping ambitious people and brands create aesthetically pleasing web app experiences.', image: {url: 'www.leroyclarkejr.com/images/leroyclarkejr.jpg', width='800',  height:'772', alt='Leroy Clarke Jr. in black and white.'}, site_name: 'LeroyClarkeJr'}}
       />
       <Header />
       <main id="layout-content">{children}</main>
