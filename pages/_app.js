be99a3b1729537ca7af5ac5f6a3775735aea87d3
Leroy Clarke Jr <lclarkejr94@gmail.com>
2021-08-11 01:59:03 -0400

    added google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,6 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
 import "tailwindcss/tailwind.css";
 import "../styles/global.scss";
 
@@ -7,6 +10,19 @@ import { DefaultSeo } from "next-seo";
 import SEO from "../next-seo.config";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      window.gtag("config", process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
+        page_path: url,
+      });
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
       <Head>
--- pages/portfolio.js
+++ pages/portfolio.js
@@ -4,7 +4,7 @@ import getProjects from "../lib/airtable";
 import Image from "next/image";
 
 export default function Portfolio({ projects }) {
-  console.log(projects);
+  // console.log(projects);
 
   return (
     <Layout>
@@ -35,15 +35,16 @@ export default function Portfolio({ projects }) {
                     <h5 className="mt-3">Stack</h5>
                     <p>{project.fields.Stack}</p>
                     {project.fields.Url === "#" ? (
-                      <span className="my-button url-link">Coming soon!</span>
+                      <h5 className="my-button url-link mt-3">Coming soon!</h5>
                     ) : (
                       <a
                         href={project.fields.Url}
-                        className="url-link my-button flex flex-nowrap flex-row mt-3"
+                        className="url-link my-button flex flex-nowrap flex-row items-center mt-3"
                       >
+                        <h5> See live </h5>
                         <svg
                           xmlns="http://www.w3.org/2000/svg"
-                          className="h-5 w-5 mr-1"
+                          className="h-4 w-4 ml-1"
                           viewBox="0 0 20 20"
                           fill="currentColor"
                         >
@@ -52,8 +53,7 @@ export default function Portfolio({ projects }) {
                             d="M12.586 4.586a2 2 0 112.828 2.828l-3 3a2 2 0 01-2.828 0 1 1 0 00-1.414 1.414 4 4 0 005.656 0l3-3a4 4 0 00-5.656-5.656l-1.5 1.5a1 1 0 101.414 1.414l1.5-1.5zm-5 5a2 2 0 012.828 0 1 1 0 101.414-1.414 4 4 0 00-5.656 0l-3 3a4 4 0 105.656 5.656l1.5-1.5a1 1 0 10-1.414-1.414l-1.5 1.5a2 2 0 11-2.828-2.828l3-3z"
                             clipRule="evenodd"
                           />
-                        </svg>{" "}
-                        See live
+                        </svg>
                       </a>
                     )}
                   </div>
