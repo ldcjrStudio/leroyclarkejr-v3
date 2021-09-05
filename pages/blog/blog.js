4336b8d15dc9238d0887bdaaa88fca80ce5ffb16
Leroy Clarke Jr <lclarkejr94@gmail.com>
2021-09-05 17:38:08 -0400

    deleted blog page

--- pages/blog/blog.js
+++
@@ -1,9 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-export default function blogAPI(req, res) {
-  if (req.method === "POST") {
-    //process post request
-  } else {
-    //handle any other http method
-  }
-}
